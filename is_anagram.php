fd61c0f84fb71beeb658aeacbdde6eca454e2b74
archlicher <christophergbbg@yahoo.com>
2019-12-19 19:09:03 +0200

    is_anagram(A, B) - solved

---
+++ is_anagram.php
@@ -0,0 +1,16 @@
+<?php
+
+function is_anagram($a, $b) {
+	$arr_a = str_split($a);
+	$arr_b = str_split($b);
+
+	sort($arr_a);
+	sort($arr_b);
+
+	if (join("", $arr_a) == join("", $arr_b)) {
+		echo "true";
+	} else  { echo "false"; }
+}
+
+is_anagram("silent", "listen");
+is_anagram("letter", "silent");
