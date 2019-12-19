15629e5de6db238909034df88c6b3b49376a1b37
archlicher <christophergbbg@yahoo.com>
2019-12-19 23:40:45 +0200

    fix equals sign

--- is_anagram.php
+++ is_anagram.php
@@ -7,10 +7,13 @@ function is_anagram($a, $b) {
 	sort($arr_a);
 	sort($arr_b);
 
-	if (join("", $arr_a) == join("", $arr_b)) {
+	if (join("", $arr_a) === join("", $arr_b)) {
 		echo "true";
 	} else  { echo "false"; }
 }
 
 is_anagram("silent", "listen");
 is_anagram("letter", "silent");
+is_anagram("letter", 123456);
+is_anagram("letter", 12345);
+is_anagram("lette", "123sil");
